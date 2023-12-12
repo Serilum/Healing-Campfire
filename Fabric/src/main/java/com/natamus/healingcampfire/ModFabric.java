d5064f0e56ee9ee93a1bc48fa0c98b8f46838cb6
Rick South <rick@natamus.com>
2023-12-12 09:58:22 +0100

    Healing Campfire source code for 1.19.2.

---
+++ Fabric/src/main/java/com/natamus/healingcampfire/ModFabric.java
@@ -0,0 +1,32 @@
+package com.natamus.healingcampfire;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
+import com.natamus.healingcampfire.events.CampfireEvent;
+import com.natamus.healingcampfire.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectivePlayerEvents.PLAYER_TICK.register((ServerLevel world, ServerPlayer player) -> {
+			CampfireEvent.playerTickEvent(world, player);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
