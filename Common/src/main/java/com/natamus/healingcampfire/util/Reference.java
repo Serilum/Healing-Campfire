79551cc480d531c45b0e980406b4ce847db7eab7
Rick South <rick@natamus.com>
2024-04-28 15:59:23 +0200

    Healing Campfire source code for 1.20.5.

---
+++ Common/src/main/java/com/natamus/healingcampfire/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.healingcampfire.util;
+
+public class Reference {
+	public static final String MOD_ID = "healingcampfire";
+	public static final String NAME = "Healing Campfire";
+	public static final String VERSION = "5.3";
+	public static final String ACCEPTED_VERSIONS = "[1.20.5]";
+}
---
+++ NeoForge/src/main/java/com/natamus/healingcampfire/neoforge/events/NeoForgeCampfireEvent.java
@@ -0,0 +1,24 @@
+package com.natamus.healingcampfire.neoforge.events;
+
+import com.natamus.healingcampfire.events.CampfireEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeCampfireEvent {
+	@SubscribeEvent
+	public static void playerTickEvent(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide) {
+			return;
+		}
+
+		CampfireEvent.playerTickEvent((ServerLevel)level, (ServerPlayer)player);
+	}
+}
