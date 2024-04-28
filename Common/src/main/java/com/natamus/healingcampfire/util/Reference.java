9c5aa624ae77cc763e94f657562ec269ca6f5896
Rick South <rick@natamus.com>
2024-04-28 19:00:51 +0200

    (v5.4) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- Common/src/main/java/com/natamus/healingcampfire/util/Reference.java
+++ Common/src/main/java/com/natamus/healingcampfire/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.healingcampfire.util;
 public class Reference {
 	public static final String MOD_ID = "healingcampfire";
 	public static final String NAME = "Healing Campfire";
-	public static final String VERSION = "5.3";
+	public static final String VERSION = "5.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.5]";
 }
--- NeoForge/src/main/java/com/natamus/healingcampfire/neoforge/events/NeoForgeCampfireEvent.java
+++ NeoForge/src/main/java/com/natamus/healingcampfire/neoforge/events/NeoForgeCampfireEvent.java
@@ -4,16 +4,15 @@ import com.natamus.healingcampfire.events.CampfireEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
-import net.neoforged.bus.api.SubscribeEvent;
+import net.minecraft.world.level.Level;import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
 @EventBusSubscriber
 public class NeoForgeCampfireEvent {
 	@SubscribeEvent
-	public static void playerTickEvent(PlayerTickEvent e) {
-		Player player = e.player;
+	public static void playerTickEvent(PlayerTickEvent.Post e) {
+		Player player = e.getEntity();
 		Level level = player.level();
 		if (level.isClientSide) {
 			return;
