eacf878ac229c92d842f8a8b881a45612184724b
Rick South <rick@natamus.com>
2024-12-06 13:28:25 +0100

    Healing Campfire source code for 1.21.4.

---
+++ Forge/src/main/java/com/natamus/healingcampfire/forge/events/ForgeCampfireEvent.java
@@ -0,0 +1,24 @@
+package com.natamus.healingcampfire.forge.events;
+
+import com.natamus.healingcampfire.events.CampfireEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeCampfireEvent {
+	@SubscribeEvent
+	public void playerTickEvent(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide) {
+			return;
+		}
+
+		CampfireEvent.playerTickEvent((ServerLevel)level, (ServerPlayer)player);
+	}
+}
