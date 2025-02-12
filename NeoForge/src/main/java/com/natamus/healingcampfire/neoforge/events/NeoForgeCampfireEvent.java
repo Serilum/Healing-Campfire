65848af99701540ba353cfe5f3c7a61d6419f03e
Rick South <rick@natamus.com>
2025-02-12 23:30:23 +0100

    (v6.2) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/healingcampfire/neoforge/events/NeoForgeCampfireEvent.java
+++ NeoForge/src/main/java/com/natamus/healingcampfire/neoforge/events/NeoForgeCampfireEvent.java
@@ -5,10 +5,8 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
-@EventBusSubscriber
 public class NeoForgeCampfireEvent {
 	@SubscribeEvent
 	public static void playerTickEvent(PlayerTickEvent.Post e) {
